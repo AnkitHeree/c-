b0b4bde47dfbdbe9dca64f4838e22136a3633b0d
AnkitHeree <ankityadav9082384811@gmail.com>
2026-09-09 15:33:30 +0530

    Its Just a Peasce of code

---
+++ Ankit.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 21;
+        string name = "Ankit";
+        double height = 5.8;
+        bool isStudent = true;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Height: " + height);
+        Console.WriteLine("Student: " + isStudent);
+    }
+}
